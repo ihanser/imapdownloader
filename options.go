1a299cedfe51e82dfc7276207acf8b6d10ede771
ihanser <68424846+ihanser@users.noreply.github.com>
2026-06-27 19:37:12 +0800

    add Threads field

--- options.go
+++ options.go
@@ -13,7 +13,8 @@ type Options struct {
 	Username string   `yaml:"username"`
 	Password string   `yaml:"password"`
 	Prefixes []string `yaml:"prefixes"`
-	Parallel int      `yaml:"parallel"` // ★ 并行下载数（新增）
+	Parallel int      `yaml:"parallel"` // 并行下载文件夹数
+	Threads  int      `yaml:"threads"`  // 单个文件夹内下载线程数
 	absDir   string   // 绝对路径（内部使用）
 }
 
@@ -21,7 +22,8 @@ func (o *Options) print() {
 	log.Infof("====== 配置信息 ======")
 	log.Infof("用户名：%s", o.Username)
 	log.Infof("服务器地址：%s", o.Host)
-	log.Infof("并行连接数：%d", o.Parallel)
+	log.Infof("并行文件夹数：%d", o.Parallel)
+	log.Infof("文件夹内线程数：%d", o.Threads)
 	for _, prefix := range o.Prefixes {
 		log.Infof("邮箱文件夹：%s", prefix)
 	}
